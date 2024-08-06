1ac60d305ef42487dd1edde1c5a09951b7a2e047
viperluan <viperluan@gmail.com>
2024-08-06 17:45:04 -0300

    cria containers para conversores

---
+++ src/components/ArabicToRomanContainer/index.tsx
@@ -0,0 +1,66 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { Input } from "../Input";
+import { ArabicToRoman } from "../../entities/models/ArabicToRoman";
+import { ArabicToRomanValidator } from "../../entities/models/ArabicToRomanValidator";
+import { ArabicToRomanConverter } from "../../entities/models/ArabicToRomanConverter";
+
+const ArabicToRomanContainer = () => {
+  const [inputArabicNumber, setInputArabicNumber] = useState("");
+  const [romanNumber, setRomanNumber] = useState("");
+
+  const handleRomanNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const typedValue = event.target.value
+      .replace(/\D/gi, "")
+      .toLocaleUpperCase();
+
+    setInputArabicNumber(typedValue);
+  };
+
+  const handleArabicNumberConverter = (arabicNumber: string) => {
+    try {
+      const arabicToRomanValidator = new ArabicToRomanValidator();
+      const arabicToRomanConverter = new ArabicToRomanConverter();
+
+      const arabicToRoman = new ArabicToRoman(
+        arabicToRomanValidator,
+        arabicToRomanConverter,
+      );
+
+      const newArabicNumber = arabicToRoman.execute(arabicNumber);
+
+      setRomanNumber(String(newArabicNumber));
+    } catch (error: Error | unknown) {
+      if (error instanceof Error) {
+        setRomanNumber(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log(inputArabicNumber);
+    handleArabicNumberConverter(inputArabicNumber);
+  }, [inputArabicNumber]);
+
+  return (
+    <section>
+      <h2 className="text-2xl">Números Arábicos para Romanos</h2>
+
+      <div className="p-8">
+        <Input
+          className="py-2 px-4"
+          placeholder="Digite um número arábico"
+          value={inputArabicNumber}
+          onInput={handleRomanNumberChange}
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <p className="mb-4">Seu número convertido é:</p>
+
+        <p className="text-4xl">{romanNumber}</p>
+      </div>
+    </section>
+  );
+};
+
+export { ArabicToRomanContainer };
---
+++ src/components/RomanToArabicContainer/index.tsx
@@ -0,0 +1,65 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { Input } from "../Input";
+import { RomanToArabic } from "../../entities/models/RomanToArabic";
+import { RomanToArabicValidator } from "../../entities/models/RomanToArabicValidator";
+import { RomanToArabicConverter } from "../../entities/models/RomanToArabicConverter";
+
+const RomanToArabicContainer = () => {
+  const [inputRomanNumber, setInputRomanNumber] = useState("");
+  const [arabicNumber, setArabicNumber] = useState("");
+
+  const handleRomanNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const typedValue = event.target.value
+      .replace(/[^IVXLCDM]/gi, "")
+      .toLocaleUpperCase();
+
+    setInputRomanNumber(typedValue);
+  };
+
+  const handleArabicNumberConverter = (romanNumber: string) => {
+    try {
+      const romanToArabicValidator = new RomanToArabicValidator();
+      const romanToArabicConverter = new RomanToArabicConverter();
+
+      const romanToArabic = new RomanToArabic(
+        romanToArabicValidator,
+        romanToArabicConverter,
+      );
+
+      const newArabicNumber = romanToArabic.execute(romanNumber);
+
+      setArabicNumber(String(newArabicNumber));
+    } catch (error: Error | unknown) {
+      if (error instanceof Error) {
+        setArabicNumber(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleArabicNumberConverter(inputRomanNumber);
+  }, [inputRomanNumber]);
+
+  return (
+    <div>
+      <h2 className="text-2xl">Números Romanos para Arábicos</h2>
+
+      <div className="p-8">
+        <Input
+          className="py-2 px-4"
+          placeholder="Digite um número romano"
+          value={inputRomanNumber}
+          onInput={handleRomanNumberChange}
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <p className="mb-4">Seu número convertido é:</p>
+
+        <p className="text-4xl">{arabicNumber}</p>
+      </div>
+    </div>
+  );
+};
+
+export { RomanToArabicContainer };
