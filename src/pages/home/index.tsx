41b87521272045c1b33d8029e7d6490dcf23ea24
viperluan <viperluan@gmail.com>
2024-08-06 10:04:18 -0300

    adiciona funcionalidades pagina home e converter

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,10 +3,18 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <main className="h-full flex flex-col items-center justify-center gap-4 p-4">
+        <h1 className="text-5xl">Aplicações</h1>
 
-      <Link to="converter">Converter</Link>
-      <Link to="treemap">TreeMap</Link>
+        <nav className="flex flex-col w-full gap-2 border border-slate-50 rounded">
+          <Link className="hover:bg-slate-400 p-4" to="converter">
+            Converter
+          </Link>
+          <Link className="hover:bg-slate-400 p-4" to="treemap">
+            TreeMap
+          </Link>
+        </nav>
+      </main>
     </>
   );
 };
