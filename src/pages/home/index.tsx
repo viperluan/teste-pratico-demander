f9fc2cf791f358afba24755454ea11853c7fef31
viperluan <viperluan@gmail.com>
2024-08-06 16:08:51 -0300

    adiciona margem e mediaquery

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,9 +4,9 @@ const Home = () => {
   return (
     <>
       <main className="h-full flex flex-col items-center justify-center gap-4 p-4">
-        <h1 className="text-5xl">Aplicações</h1>
+        <h1 className="text-5xl mb-8">Aplicações</h1>
 
-        <nav className="flex flex-col w-full gap-2 border border-slate-50 rounded">
+        <nav className="flex flex-col w-full md:w-2/4 gap-2 border border-slate-50 rounded">
           <Link className="hover:bg-slate-400 p-4" to="converter">
             Converter
           </Link>
