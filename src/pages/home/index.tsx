7e533d8749852ef9a666cc99e32b09afbde8626b
viperluan <viperluan@gmail.com>
2024-08-06 19:03:58 -0300

    atualiza estilização dos botões

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,10 +7,10 @@ const Home = () => {
         <h1 className="text-5xl mb-8">Aplicações</h1>
 
         <nav className="flex flex-col w-full md:w-2/4 gap-2 border border-slate-50 rounded">
-          <Link className="hover:bg-slate-400 p-4" to="converter">
+          <Link className="hover:bg-gray-600 p-4" to="converter">
             Converter
           </Link>
-          <Link className="hover:bg-slate-400 p-4" to="treemap">
+          <Link className="hover:bg-gray-600 p-4" to="treemap">
             TreeMap
           </Link>
         </nav>
